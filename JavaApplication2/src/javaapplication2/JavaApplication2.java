6a6e3142995f1d2e1ff69b85ebcaec675d1a5efa
Elizabeth Jean Matchefts <Elizabeth Jean Matchefts>
2015-11-09 00:27:59 -0500

    Deleted further comments

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -11,9 +11,6 @@ package javaapplication2;
  */
 public class JavaApplication2 {
 
-    /**
-     * @param args the command line arguments
-     */
 
    public static void main(String[] args) { 
       System.out.println("Hello, World");
